2ec3d283e997bc769f063320dd2df993d174fde0
Norman Jäckel <openslides@normanjaeckel.de>
2016-09-30 23:39:42 +0200

    Refactored projector requirements system for autoupdate.

--- openslides/assignments/projector.py
+++ openslides/assignments/projector.py
@@ -46,7 +46,10 @@ class AssignmentSlide(ProjectorElement):
                     for option in poll.options.all():
                         yield option.candidate
 
-    def need_full_update_for_this(self, collection_element):
+    def get_collections_required_for_this(self, collection_element, config_entry, **information):
+        output = super().get_collections_required_for_this(collection_element, config_entry, **information)
         # Full update if assignment changes because then we may have new
         # candidates and therefor need new users.
-        return collection_element.collection_string == Assignment.get_collection_string()
+        if collection_element.collection_string == Assignment.get_collection_string() and collection_element.id == config_entry.get('id'):
+            output.extend(self.get_requirements_as_collections(config_entry))
+        return output
